0b2c219273ae3176ced0cdc724309a7350352a8b
Alexis Fair <aurorarosewyn@gmail.com>
2026-09-02 00:15:53 -0400

    Basic camera moving

--- src/components/MikuPage.js
+++ src/components/MikuPage.js
@@ -5,10 +5,12 @@ import "./MikuPage.css";
 
 function MikuPage() {
     // Static values for changing animation things
-    const X_CORRECTION = .40; // How much of the left side of the screen to use. This is 40%
     const INIT_OFFSET = 10;
     const ZCORRECTION = 0.05;
     const STAR_ROTATION_SPEED_INVERSE = 8.0;
+    const NORMAL_STAR_SIZE = 300;
+    const ZOOM_TIME = 1.0;
+    const X_OFFSET_FROM_CENTER = 25;
 
     // Enum/state values
     const STATE_NO_RENDER = 1; // Don't render. Either too far away in song or completely done
@@ -21,6 +23,7 @@ function MikuPage() {
     // Use useState for things that need to re-render on change and are NOT time sensitive relative to other things in the same frame
     const [loadingText, setLoadingText] = useState("Miku!");
     const [lyrics, setLyrics] = useState("");
+    const [debug, setDebug] = useState("");
     const [songLoaded, setSongLoaded] = useState(false);
     const [curPosition, setCurPosition] = useState(0);
     const [forceRender, setForceRender] = useState(0);
@@ -41,11 +44,11 @@ function MikuPage() {
     // Values for rendering lyricStars
     const curPhraseId = useRef(0);
 
-    const [leftX, setLeftX] = useState(0);
-    const [rightX, setRightX] = useState(100);
-    const [topY, setTopY] = useState(0);
-    const [bottomY, setBottomY] = useState(100);
-    const [curZ, setCurZ] = useState(0);
+    const leftX = useRef(0);
+    const rightX = useRef(100);
+    const topY = useRef(0);
+    const bottomY = useRef(100);
+    const curZ = useRef(0);
 
 
     function RandBetween(min, max) {
@@ -63,43 +66,114 @@ function MikuPage() {
         rotation = 0;
         phrase = null;
         chars = [];
+        state = STATE_NO_RENDER;
+
+        lastTime = 0;
+        xDist = 0;
+        yDist = 0;
+        zDist = 0;
+        totalZoom = 0;
+
         animate(curTime) {
-            // Set the needed variables here for stuff like state machine, etc..
+            // Make the one we're currently on responsible for re-render
             if (this.phraseId == curPhraseId.current) {
                 setForceRender(prev => prev + 1);
             }
+
+            if(this.state == STATE_NO_RENDER) {
+                // Don't do anything
+            }
+            else if(this.state == STATE_BACKGROUND) {
+                // Check for if we are the zooming to one
+                if(this.phraseId == curPhraseId.current) {
+                    // Change to ZOOMING_TO !!!
+                    this.state = STATE_ZOOMING_TO;
+
+                    // Calculate how much we need to move center X total
+                    let centerX = (leftX.current + rightX.current)/2.0;
+                    let centerY = (topY.current + bottomY.current)/2.0;
+
+                    this.xDist = (this.baseX + X_OFFSET_FROM_CENTER) - centerX;
+                    this.yDist = this.baseY - centerY;
+                    this.zDist = this.baseZ - curZ.current;
+                }
+            }
+            else if(this.state == STATE_MAIN_STAGE) {
+                // setDebug("HI! :"+this.phraseId + " | " + this.chars[this.chars.length-1].endTime+" | "+leftX+" "+topY);
+                // Check for when to move to LEAVING & increment curPhraseId
+                if(this.chars[this.chars.length-1].endTime < curTime) {
+                    curPhraseId.current = curPhraseId.current + 1;
+                    this.state = STATE_LEAVING;
+                }
+            }
+            else if(this.state == STATE_ZOOMING_TO) {
+                // Check for when we're done and in MAIN_STAGE
+                let dt = (curTime-this.lastTime)/1000.0; // dt is in SECONDS now
+                this.totalZoom = this.totalZoom + dt;
+                let deltaX = this.xDist * (dt / ZOOM_TIME);
+                let deltaY = this.yDist * (dt / ZOOM_TIME);
+                let deltaZ = this.zDist * (dt / ZOOM_TIME);
+                setDebug(this.phraseId+" Zoom!: "+this.xDist+" "+this.yDist+" "+this.zDist+"  DeltaX: "+deltaX+" "+leftX.current);
+                leftX.current += deltaX;
+                rightX.current += deltaX;
+                bottomY.current += deltaY;
+                topY.current += deltaY;
+                curZ.current += deltaZ;
+
+                if(this.totalZoom >= ZOOM_TIME) {
+                    this.state = STATE_MAIN_STAGE;
+                }
+            }
+            else if(this.state == STATE_LEAVING) {
+            
+            }
+            // Check lyrics
+
             this.rotation = this.rotationOffset + (this.rotationParity * curTime/STAR_ROTATION_SPEED_INVERSE);
+            this.lastTime = curTime;
         }
 
         render() {
-            let realX = this.baseX - leftX;
-            let realY = this.baseY - topY;
-            // if(realX <= 0 || realX >= 100 || realY <= 0 || realY >= 100) {
-            //     return <div />
-            // }
-
-            // X from 0 to 100 is more like 0 to 40 in the real thing, so multiply X by 0.4
-            realX = realX * X_CORRECTION;
+            let realX = this.baseX - leftX.current;
+            let realY = this.baseY - topY.current;
 
             // Based on how far away we are from the current phrase (phraseId - curPhraseId, send the coordinates a bit more towards center (leftX+rightX)/2). Linear or slow exponential? 
             // (simulating FOV stuff)
             // WILL NEED TO HANDLE SUDDEN TRANSITION -> MAKE IT SMOOTH SOMEHOW
 
-            let zDiff = this.baseZ - curZ;
-            let percentCorrection = ZCORRECTION * phraseDiff;
-            if (percentCorrection < 0) percentCorrection = 0;
+            let zDiff = this.baseZ - curZ.current;
+            let percentCorrection = ZCORRECTION * zDiff;
+            if (percentCorrection < -1) percentCorrection = -1;
             if (percentCorrection > 1) percentCorrection = 1;
 
             // // Move percentCorrection towards center
-            let centerX = (leftX + rightX)/2.0;
-            let centerY = (topY + bottomY)/2.0;
+            let centerX = (leftX.current + rightX.current)/2.0;
+            let centerY = (topY.current + bottomY.current)/2.0;
             realX = realX + (centerX - realX) * percentCorrection;
             realY = realY + (centerY - realY) * percentCorrection;
+
+            // Now change size based off of zDiff
+            // If zDiff < -1, should be 0. This star has already moved 'behind' camera
+            // if zDiff > 3, should be 0. Only wanna show first 3 stars??
+            let sizeMult = 0;
+            if (zDiff < 0) {
+                sizeMult = -2.0 * zDiff + 1;
+            } else {
+                sizeMult = (1.0/(10*zDiff + 1));
+            }
+            
+            // If too far away/not supposed to be visible, return nothing
+            if(sizeMult < 0) {
+                return <div />
+            }
+            // Cap at 3x size
+            if(sizeMult > 3) sizeMult = 3;
+            this.size = sizeMult * NORMAL_STAR_SIZE;
 
             return (
                 <div>
                     {/* <div> Test! {this.baseX} {this.baseY} {realX} {realY} </div> */}
-                    <img src="/images/Star.png" alt="Star" style={{position: "absolute", left: `${realX}%`, top: `${realY}%`, width: "30px", transform: `translate(-50%, -50%) rotate(${(this.rotation)%360}deg)`}}/>
+                    <img src="/images/Star.png" alt="Star" style={{position: "absolute", left: `${realX}%`, top: `${realY}%`, width: `${this.size}px`, transform: `translate(-50%, -50%) rotate(${(this.rotation)%360}deg)`}}/>
                 </div>
             )
         }
@@ -195,8 +269,11 @@ function MikuPage() {
                 i = 0;
 
                 let p = player.video.firstPhrase;
-                let curX = 50.0 + RandBetween(-INIT_OFFSET, INIT_OFFSET);
+                let curX = (50.0 - X_OFFSET_FROM_CENTER) + RandBetween(-INIT_OFFSET, INIT_OFFSET);
                 let curY = 50.0 + RandBetween(-INIT_OFFSET, INIT_OFFSET);
+                let curCenterX = 50;
+                let curCenterY = 50;
+
                 while(p) {
                     phraseToIndex.current.set(p, i);
                     // Make the lyricStar object
@@ -210,6 +287,12 @@ function MikuPage() {
                     lyricStar.baseZ = i;
                     lyricStar.rotationOffset = RandBetween(0, 360);
                     lyricStar.rotation = lyricStar.rotationOffset;
+                    if (i == 0) {
+                        lyricStar.state = STATE_MAIN_STAGE;
+                    } else {
+                        lyricStar.state = STATE_BACKGROUND;
+                    }
+
                     if (RandBetween(0, 1) < 0.5) {
                         lyricStar.rotationParity = 1;
                     }
@@ -219,6 +302,7 @@ function MikuPage() {
                     let pc = p.firstChar;
                     while(pc) {
                         lyricStar.chars.push(pc);
+                        if (pc == p.lastChar) break;
                         pc = pc.next;
                     }
                     p.animate = animatePhrase;
@@ -234,8 +318,14 @@ function MikuPage() {
                         nextY = RandBetween(5, 95);
                     }
                     
-                    curX = curX + (nextX - 50);
-                    curY = curY + (nextY - 50);
+                    // Next star will be the current center of screen's coords + the random offset from -50 to 50
+                    curX = curCenterX + (nextX - 50);
+                    curY = curCenterY + (nextY - 50);
+
+                    // Next center will be +25 in x from where the star is, and keep same Y
+                    curCenterX = curX - X_OFFSET_FROM_CENTER;
+                    curCenterY = curY;
+
                     p = p.next;
                     i++;
                 }
@@ -289,6 +379,9 @@ function MikuPage() {
             {/* <img className="starImage" src="/images/Star.png" alt="Star" style={{width: "300px", transform: `translate(-50%, -50%) rotate(${(-curPosition/8.0)%360}deg)`}}/> */}
             <div className="lyrics">
                 {lyrics}
+            </div>
+            <div className="lyrics">
+                {debug}
             </div>
             {lyricStarList.current.map((star, index) => star.render())}
         </div>
